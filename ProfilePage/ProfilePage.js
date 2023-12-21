2f52082fc81281e1318ab8990d6866e9bfd8daf9
KoichiMin <koichidminami@gmail.com>
2023-12-21 14:00:48 -0500

    feature post fetch inside modal

--- ProfilePage/ProfilePage.js
+++ ProfilePage/ProfilePage.js
@@ -1,12 +1,30 @@
 "use strict"
 
+const sendMessage = document.getElementById("send-message");
+const messageText = document.getElementById("message-text");
 
 window.onload = () =>{
+    sendMessage.onclick = sendPost;
 
 }
 
-const messageText = document.getElementById("message-text");
 
-const SendPost = () =>{
-
+const sendPost = () =>{
+    fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts", 
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                
+            },
+            body: JSON.stringify({
+                text: messageText.value
+            }),
+        }
+    )
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data);
+        })
+        .catch(err => console.log(err));
 }
