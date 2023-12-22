66d9f9695d54ac392d7b15c61726b40a07211512
bp442 <bpark135@gmail.com>
2023-12-22 14:06:59 -0500

    added login functionality

--- landing/landing.js
+++ landing/landing.js
@@ -2,33 +2,23 @@
 "use strict";
 
 //login API
-const apiBaseURL = "https://microbloglite.herokuapp.com";
+const apiBaseURL = "microbloglite.us-east-2.elasticbeanstalk.com";
 
+const loginButton = document.getElementById("loginButton")
 const loginForm = document.querySelector("#login");
 
-window.onload() = init;
+window.onload = init;
 
-function init(){
-    if(isLoggedIn){
-        window.location.href = '/posts';
-    }
-    else{
-    //code to setup the nav bar to log out
-    const navLogout = document.getElementById("navLogout");
-    navLogout.onclick = logout;
-    }
+function init() {
+        loginButton.onclick = loginClick;
 }
 
-loginForm.onsubmit = function (event) {
-    // Prevent the form from refreshing the page,
-    // as it will do by default when the Submit event is triggered:
-    event.preventDefault();
-
-    // We can use loginForm.username (for example) to access
-    // the input element in the form which has the ID of "username".
+function loginClick(){
+    const usernameField = document.getElementById("username");
+    const passwordField = document.getElementById("password");
     const loginData = {
-        username: loginForm.username.value,
-        password: loginForm.password.value,
+        username: usernameField.value,
+        password: passwordField.value,
     }
 
     // Disables the button after the form has been submitted already:
@@ -36,10 +26,10 @@ loginForm.onsubmit = function (event) {
 
     // Time to actually process the login using the function from auth.js!
     login(loginData);
-};
+}
 
 
-function getLoginData () {
+function getLoginData() {
     const loginJSON = window.localStorage.getItem("login-data");
     return JSON.parse(loginJSON) || {};
 }
@@ -47,7 +37,7 @@ function getLoginData () {
 
 // You can use this function to see whether the current visitor is
 // logged in. It returns either `true` or `false`.
-function isLoggedIn () {
+function isLoggedIn() {
     const loginData = getLoginData();
     return Boolean(loginData.token);
 }
@@ -57,9 +47,9 @@ function isLoggedIn () {
 // landing page, in order to process a user's login. READ this code,
 // and feel free to re-use parts of it for other `fetch()` requests
 // you may need to write.
-function login (loginData) {
+function login(loginData) {
     // POST /auth/login
-    const options = { 
+    const options = {
         method: "POST",
         headers: {
             // This header specifies the type of content we're sending.
@@ -68,47 +58,13 @@ function login (loginData) {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(loginData),
+        
     };
 
-    return fetch(apiBaseURL + "/auth/login", options)
-        .then(response => response.json())
-        .then(loginData => {
+    return fetch("http://microbloglite.us-east-2.elasticbeanstalk.com/auth/login", options).then(response => response.json()).then(loginData => {
             window.localStorage.setItem("login-data", JSON.stringify(loginData));
             window.location.assign("/posts");  // redirect
 
             return loginData;
-        });
-}
-
-
-// This is the `logout()` function you will use for any logout button
-// which you may include in various pages in your app. Again, READ this
-// function and you will probably want to re-use parts of it for other
-// `fetch()` requests you may need to write.
-function logout () {
-    const loginData = getLoginData();
-
-    // GET /auth/logout
-    const options = { 
-        method: "GET",
-        headers: { 
-            // This header is how we authenticate our user with the
-            // server for any API requests which require the user
-            // to be logged-in in order to have access.
-            // In the API docs, these endpoints display a lock icon.
-            Authorization: `Bearer ${loginData.token}`,
-        },
-    };
-
-    fetch(apiBaseURL + "/auth/logout", options)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .finally(() => {
-            // We're using `finally()` so that we will continue with the
-            // browser side of logging out (below) even if there is an 
-            // error with the fetch request above.
-
-            window.localStorage.removeItem("login-data");  // remove login data from LocalStorage
-            window.location.assign("/");  // redirect back to landing page
         });
 }
