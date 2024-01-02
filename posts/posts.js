32ec9bcf8138f038c1e1a4280034270811881a27
bp442 <bpark135@gmail.com>
2024-01-02 10:40:02 -0500

    display posts function completed with edited timestamps

--- posts/posts.js
+++ posts/posts.js
@@ -1,32 +1,33 @@
 /* Posts Page JavaScript */
 "use strict";
 
+let loginData = getLoginData();
+let options = {
+  method: "GET",
+  headers: {
+    Authorization: `Bearer ${loginData.token}`,
+    'Content-Type': 'application/json',
+  },
+};
 
-
-let apiLink = "https://microbloglite.us-east-2.elasticbeanstalk.com";
+let apiLink = "http://microbloglite.us-east-2.elasticbeanstalk.com/api/posts";
 
 let cardContainer = document.getElementById("displayPostsContainer");
 
 window.onload = init();
 
-function init() {
+function init(){
   loadPosts();
 }
 
 function loadPosts() {
-
-  let loginData = getLoginData();
-  let options = {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${loginData.token}`,
-    },
-  };
-
-  fetch(apiLink + "/api/posts", options)
+  fetch(apiLink, options)
     .then(response => response.json())
     .then(userPosts => {
-      createCard(userPosts);
+      for(let x = 0; x < userPosts.length; x++){
+        createCard(userPosts[x]);
+      }
+      
     });
 
 }
@@ -37,6 +38,9 @@ function createCard(userPost) {
   let postTimestamp = userPost.createdAt;
   let postText = userPost.text;
 
+  let postDate = postTimestamp.substr(0, postTimestamp.indexOf('T'));
+  let postTime = postTimestamp.substring(postTimestamp.indexOf('T') + 1, postTimestamp.indexOf('.'));
+
   //create a new card every time there's a new post
   let card = document.createElement('div');
   card.className = 'card';
@@ -46,7 +50,7 @@ function createCard(userPost) {
   cardBody.className = 'card-body';
   //add body content
   cardBody.innerHTML = `<h5 class="card-title">${postUsername}</h5>` +
-    `<h6 class="card-subtitle mb-2 text-body-secondary">${postTimestamp}</h6>` +
+    `<h6 class="card-subtitle mb-2 text-body-secondary">${postDate}, ${postTime}</h6>` +
     `<p class="card-text">${postText}</p>`;
 
   //Append card body to card
@@ -57,7 +61,11 @@ function createCard(userPost) {
 }
 
 function getLoginData() {
+  console.log("getLoginData");
   const loginJSON = window.localStorage.getItem("login-data");
+  console.log(loginJSON);
+  console.log(JSON.parse(loginJSON));
+
   return JSON.parse(loginJSON) || {};
 }
 
